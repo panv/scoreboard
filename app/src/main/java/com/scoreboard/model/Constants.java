760f0b89fc2ebd22d9e36749eb21ab7308d50482
panv <panv@hotmail.fr>
2017-02-14 14:11:55 +0100

    Add constants class

---
+++ app/src/main/java/com/scoreboard/model/Constants.java
@@ -0,0 +1,13 @@
+package com.scoreboard.model;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final int JASS_DEFAULT_TEAM_COUNT = 2;
+    public static final int JASS_TOTAL_POINTS_IN_ROUND = 157;
+    public static final int JASS_MATCH_POINTS = 257;
+    public static final int JASS_DEFAULT_POINTS_GOAL = 1000;
+
+}
